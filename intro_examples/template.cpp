8983a30576d8344ce1646d17ca9ef131dad08d61
Ted Wilcox <ted@purestorage.com>
2015-11-21 12:36:21 -0800

    Add template.cpp file.

---
+++ intro_examples/template.cpp
@@ -0,0 +1,21 @@
+/*
+ * template.cpp
+ *
+ * Just some boilerplate stuff to use for each file.
+ */
+
+// vim: set ai sw=4 et sm:
+
+#include <iostream>
+#include <complex>
+using namespace std;
+
+void local_func(void)
+{
+	return;
+}
+
+int main()
+{
+	local_func();
+}
