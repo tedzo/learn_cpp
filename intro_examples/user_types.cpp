e0436d4ca032c5c5d4e0342b27eb60da164519cc
Ted Wilcox <tedzow@gmail.com>
2015-11-29 12:37:12 -0800

    Add cVector class implementation to user_types.cpp.

--- intro_examples/user_types.cpp
+++ intro_examples/user_types.cpp
@@ -8,19 +8,19 @@
 #include <complex>
 using namespace std;
 
-struct Vector {
+struct sVector {
     int size;
     double *elem;
 };
 
-void vector_init(Vector & v, int s)
+void vector_init(sVector & v, int s)
 {
     v.elem = new double[s];
     v.size = s;
 }
 
-/* Fill values in a Vector.  Assume an already initialized vector is passed in */
-void read_vector(Vector & v)
+/* Fill values in a sVector.  Assume an already initialized vector is passed in */
+void read_vector(sVector & v)
 {
     cout << "Please give me " << v.size << " floating point numbers.\n";
 
@@ -28,8 +28,8 @@ void read_vector(Vector & v)
         cin >> v.elem[i];
 }
 
-/* Sum values in a Vector. */
-double sum_vector(Vector & v)
+/* Sum values in a sVector. */
+double sum_vector(sVector & v)
 {
     double sum = 0;
     for (auto i=0; i!=v.size; ++i)
@@ -38,19 +38,72 @@ double sum_vector(Vector & v)
     return sum;
 }
 
-int main()
+void svector_access(sVector v, sVector & rv, sVector *pv)
+{
+    int i1 = v.size;    // Access to pass-by-value vector
+    int i2 = rv.size;    // Access to pass-by-reference vector
+    int i3 = pv->size;   // Access by pointer to vector
+}
+
+void handle_svector()
 {
     int num_vals;
 
-    cout << "How many elements in the vector? ";
+    cout << "How many elements in the sVector? ";
     cin >> num_vals;
 
-    Vector v;
+    sVector v;
     vector_init(v, num_vals);
     read_vector(v);
 
     // double sum = read_and_sum(num_vals);
     cout << "The sum is: " << sum_vector(v) << "\n";
+}
+
+/* Use a class instead of a struct */
+
+class cVector {
+public:
+    cVector(int s): elem{new double[s]}, sz{s} {}     // construct a cVector
+    double& operator[](int i) { return elem[i]; }       // [] element access: subscripting
+    int size() { return sz; } // Let's see how this name overloading works!
+private:
+    double* elem;
+    int sz;
+};
+
+double cvector_read_and_sum(int s)
+{
+    cVector v(s);
+
+    cout << "Please give me " << s << " floating point numbers.\n";
+    for (auto i=0; i!=v.size(); ++i)
+        cin >> v[i];
+
+    double sum = 0;
+    for (auto i=0; i!=v.size(); ++i)
+        sum += v[i];
+    
+    return sum;
+}
+
+void handle_cvector()
+{
+    int num_vals;
+
+    cout << "How many elements in the cVector? ";
+    cin >> num_vals;
+
+    auto sum = cvector_read_and_sum(num_vals);
+
+    // double sum = read_and_sum(num_vals);
+    cout << "The sum is: " << sum << "\n";
+}
+
+int main()
+{
+    handle_svector();
+    handle_cvector();
 }
 
 // vim: set ai sw=4 et sm:
